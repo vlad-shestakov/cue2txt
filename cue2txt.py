2c445395c6b6fd58419c1f740e8d3b6980f0e0fe
Vladimir Shestakov <vlad-shestakov@mail.ru>
2025-01-17 22:37:05 +0300

    Добавлено описание

--- cue2txt.py
+++ cue2txt.py
@@ -1,15 +1,47 @@
-
 import sys
 import os
 import re
 from pprint import pprint
 
+
 # -----------------------------------------------------------
+'''
+Преобразует файлы cue в текстовый формат
+
+Формат запуска
+    py cue2txt.py CUE_FILE_NAME [TXT_FILE_NAME]
+
+, где
+    CUE_FILE_NAME - Путь к файлу .CUE
+    TXT_FILE_NAME - Путь к файлу вывода
+
+Формат выходного файла:
+    TRACK. PERFORMER - TITLE (INDEX 01)
+    ...
+    
+, например:
+    01. Seal 2007 - Amazing (00:00:00)
+    02. Groove Armada 2001 - Drifted (03:27:63)
+    03. The Doobie Brothers 1973 - Long Train Runnin (08:24:04)
+    04. Sheryl Crow 1986 - Everyday Is A Winding Road (11:42:44)
+    05. Groove Armada 2001 - Fogma (16:06:69)
+    06. Lady Gaga 2008 - Poker Face (23:02:00)
+    07. Netherlands Philharmonic Orchestra 2003 - Antonin Dvorak - Symphony No. 9 (27:01:05)
+    08. Pas De Deux - Tchaikovsky - The Nutcracker (38:49:56)
+    09. Groove Armada 2001 - Lazy Moon (43:07:26)
+    10. Tears For Fears 1985 - Shout (49:43:34)
+    11. Fine Young Cannibals 1989 - She Drives Me Crazy (56:20:61)
+    12. Groove Armada 2001 - Edge Hill (59:46:49)
+    13. Foreigner 1977 - Cold As Ice (66:41:69)
+    14. San Francisco Symphony - Ludwig van Beethoven - Symphony No. 5 (70:02:27)
+        
+    
+
+'''
+# -----------------------------------------------------------
+
+# Пример файла .CUE
 test_text = '''
-123
-sss
-123123
-asdasdsd
 REM GENRE "rock, demo"
 REM DATE 2022
 REM COMMENT "ALLic for nnm-club"
